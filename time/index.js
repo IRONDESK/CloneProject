1aa8ed4811b17dbddeb68393af421c23532d37af
SOOCHUL SON <87234410+IRONDESK@users.noreply.github.com>
2021-12-30 15:51:41 +0900

    feat: input value 가져오기, 시간 계산해서 출력하기

---
+++ time/index.js
@@ -0,0 +1,32 @@
+const declareCont = document.querySelector(".declare");
+const resultCont = document.querySelector(".result-container");
+const loadCont = document.querySelector(".load-container");
+const btnCont = document.querySelector(".btn-container");
+
+function calTime () {
+    const doingInput = declareCont.querySelector("#doingGet");
+    const timeInput = declareCont.querySelector("#timeGet");
+    const resultButton = btnCont.querySelector(".btn");
+
+    const doingResultTxt = resultCont.querySelector("#doingResult");
+    const timeResultTxt = resultCont.querySelector("#timeResult");
+
+    resultButton.addEventListener("click", () => {
+        if (doingInput.value == "" || timeInput.value == "") {
+            alert("값이 입력되지 않았습니다.");
+            return
+        }
+        resultCont.classList.add("off");
+        loadCont.classList.remove("off");
+        window.scrollBy(0, 1200);
+        setTimeout(() => {
+            loadCont.classList.add("off");
+            resultCont.classList.remove("off");
+            let calTime = Math.ceil(10000/timeInput.value);
+            doingResultTxt.textContent = doingInput.value;
+            timeResultTxt.textContent = calTime;
+        }, 1800);
+    })
+}
+
+calTime();
